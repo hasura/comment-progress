c2d56ccea2491a4903f4f35ded83ae41170deaf3
Vishnu Bharathi P <vishnubharathi04@gmail.com>
2021-02-15 09:14:35 +0530

    wraps in single async block

--- dist/index.js
+++ dist/index.js
@@ -46,30 +46,25 @@ async function findMatchingComment({ owner, repo, issue_number, identifier}) {
 
 
 
-try {
-  const repository = core.getInput('repository');
-  const [repoOwner, repoName] = repository.split('/');
-  if (!repoOwner || !repoName) {
-    throw new Error(`Invalid repository: ${repository}`);
-  }
-
-  const number = core.getInput('number');
-  const identifier = core.getInput('id');
-  const message = core.getInput('message');
-  const append = core.getInput('append');
-  const variables = core.getInput('variables');
-} catch (error) {
-  console.error(error);
-  core.setFailed(error.message);
-}
-
 (async () => {
-  console.log(`repository = ${repository}`);
-  console.log(`repoOwner = ${repoOwner}, repoName = ${repoName}`);
-  console.log(`id = ${identifier}`);
-  console.log(`variables = ${variables}`);
-
   try {
+    const repository = core.getInput('repository');
+    const [repoOwner, repoName] = repository.split('/');
+    if (!repoOwner || !repoName) {
+      throw new Error(`Invalid repository: ${repository}`);
+    }
+
+    const number = core.getInput('number');
+    const identifier = core.getInput('id');
+    const message = core.getInput('message');
+    const append = core.getInput('append');
+    const variables = core.getInput('variables');
+
+    console.log(`repository = ${repository}`);
+    console.log(`repoOwner = ${repoOwner}, repoName = ${repoName}`);
+    console.log(`id = ${identifier}`);
+    console.log(`variables = ${variables}`);
+
     const matchingComment = await findMatchingComment({
       owner: repoOwner,
       repo: repoName,
@@ -79,8 +74,9 @@ try {
 
     console.log(`matchingComment = ${matchingComment}`);
   } catch (error) {
+    console.error(error);
     core.setFailed(error.message);
-  }
+  }  
 })();
 
 
--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 import * as core from '@actions/core';
 import { findMatchingComment } from './comment';
 
-try {
-  const repository = core.getInput('repository');
-  const [repoOwner, repoName] = repository.split('/');
-  if (!repoOwner || !repoName) {
-    throw new Error(`Invalid repository: ${repository}`);
-  }
-
-  const number = core.getInput('number');
-  const identifier = core.getInput('id');
-  const message = core.getInput('message');
-  const append = core.getInput('append');
-  const variables = core.getInput('variables');
-} catch (error) {
-  console.error(error);
-  core.setFailed(error.message);
-}
-
 (async () => {
-  console.log(`repository = ${repository}`);
-  console.log(`repoOwner = ${repoOwner}, repoName = ${repoName}`);
-  console.log(`id = ${identifier}`);
-  console.log(`variables = ${variables}`);
-
   try {
+    const repository = core.getInput('repository');
+    const [repoOwner, repoName] = repository.split('/');
+    if (!repoOwner || !repoName) {
+      throw new Error(`Invalid repository: ${repository}`);
+    }
+
+    const number = core.getInput('number');
+    const identifier = core.getInput('id');
+    const message = core.getInput('message');
+    const append = core.getInput('append');
+    const variables = core.getInput('variables');
+
+    console.log(`repository = ${repository}`);
+    console.log(`repoOwner = ${repoOwner}, repoName = ${repoName}`);
+    console.log(`id = ${identifier}`);
+    console.log(`variables = ${variables}`);
+
     const matchingComment = await findMatchingComment({
       owner: repoOwner,
       repo: repoName,
@@ -34,6 +29,7 @@ try {
 
     console.log(`matchingComment = ${matchingComment}`);
   } catch (error) {
+    console.error(error);
     core.setFailed(error.message);
-  }
+  }  
 })();
